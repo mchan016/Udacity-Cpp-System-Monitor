6f60e5841aa35a4a1f7f81f7931042dc5d2d057c
mchan016 <mchan016@gmail.com>
2021-03-10 16:33:30 -0800

    Format output string for system uptime

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,22 @@
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+/*
+* ElapsedTime
+*
+* Input: Long int measuring seconds
+*
+* Output: Time string formatted as "HH:MM:SS"
+*/
+string Format::ElapsedTime(long seconds) 
+{
+    long secondsPerHour = 60 * 60;
+    long secondsPerMin = 60;
+
+    string hours = std::to_string(seconds / secondsPerHour);
+    seconds %= secondsPerHour;
+    string minutes = std::to_string(seconds / secondsPerMin);
+    seconds %= secondsPerMin;
+
+    return hours + ':' + minutes + ':' + std::to_string(seconds);
+}
